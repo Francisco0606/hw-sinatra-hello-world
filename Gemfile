f15a09a441cc3292456592746091d28204faadcf
Francisco Aguillon <francisco.i.aguillon01@utrgv.edu>
2023-07-26 02:19:23 +0000

    Finished

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
-ruby '>= 2.2.0'
+ruby '>= 2.6.0'
 gem 'sinatra', '>= 2.0.1'
+gem 'sinatra-flash', '0.3.0'
--- app.rb
+++ app.rb
@@ -1,6 +1,32 @@
 require 'sinatra'
+require 'sinatra/flash'
 
 class MyApp < Sinatra::Base
+
+    enable :sessions # for using cookies and flash messages
+    register Sinatra::Flash #for flash messages
+
+    get '/hello' do
+        erb :hello_form
+    end
+
+    post '/hello' do
+        @name = params["name"]
+        @last = params["lastname"]
+
+        if @name == "" || @name == nil
+            flash["message"] = "Please Enter a Name"
+            redirect '/hello'
+        else
+            session[:name] = @name
+        end
+
+        erb :hello
+    end
+
+    get '/somewhere' do
+        "You are nowhere"
+    end
 
 end
 
